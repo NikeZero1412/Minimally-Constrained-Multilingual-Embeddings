b40444edc19cc358667172ed587fdd55b4968ba6
Nitin Kishore Sai Samala <nsamala@cs.umass.edu>
2017-04-19 20:32:51 -0400

    Refining the code

--- embeddings_on_gypsum.py
+++ embeddings_on_gypsum.py
@@ -15,6 +15,8 @@ from bs4 import BeautifulSoup
 from nltk import tokenize
 from random import shuffle
 import ujson
+import gensim
+import numpy as np
 #from libraries.acs import acs
 
 # In[ ]:
@@ -67,15 +69,31 @@ class StemmingHelper(object):
 
 # In[33]:
 
-# Path to the English and Spanish Wiki Corpii
+# Path to the Language Wiki Corpii
 
 ENG_DIR = os.path.join(os.getcwd(),"data/English_Wiki")
 ESP_DIR = os.path.join(os.getcwd(),"wikiextractor/Spanish_Wiki")
-
+FRE_DIR = os.path.join(os.getcwd(),"wikiextractor/French_Wiki")
+ITA_DIR = os.path.join(os.getcwd(),"wikiextractor/Italian_Wiki")
 # File list in each directory
 
 ENG_filenames=os.listdir(ENG_DIR)
-ESP_filenames=os.listdir(ENG_DIR)
+ESP_filenames=os.listdir(ESP_DIR)
+FRE_filenames=os.listdir(FRE_DIR)
+ITA_filenames=os.listdir(ITA_DIR)
+
+
+# Global list variable for English sentences after processing
+LAN_sentences = []
+# Global list variable for Spanish sentences after processing
+ESP_sentences = []
+# Global list variable for French sentences after processing
+FRE_sentences = []
+# Global list variable for Italian sentences after processing
+ITA_sentences = []
+
+
+
 
 def preprocess(content):
     """
@@ -98,70 +116,45 @@ def preprocess(content):
     
     
     
-def spanishcorpus(mode):
-    # Global list variable for Spanish sentences after processing
-    ESP_sentences = []
-    if mode=="Load":
-        # Reading data back
-       print "Loading Spanish"
-       with open('espdata.json', 'r') as f:
-             ESP_sentences = ujson.load(f)
-       f.close()
-       print "Spanish Loaded" 	
-  
-    print "Done with Spanish"
-    if mode=="Save":
-        print "Going through Spanish Wiki"
-        for subdir, dirs, files in os.walk(ESP_DIR):
-            for file in files[::5]:
-                with codecs.open(os.path.join(subdir,file),'rb',encoding='utf-8') as espdoc:
-                    espcontent =espdoc.read()
-                    ESP_sentences += preprocess(espcontent)
-        # Random shuffle of the sentences
-        shuffle(ESP_sentences)
-        # Saving to file
-        print "Pickling Spanish"
-        
-        # Writing JSON data
-        with open('espdata.json', 'w') as f:
-             ujson.dump(ESP_sentences, f)
-        f.close()
-    return ESP_sentences    
-
-
-def englishcorpus(mode):
-    # Global list variable for English sentences after processing
-    ENG_sentences = []
+def parsecorpus(mode,DIR,filename,n):
+    """
+    Input parameters:
+        1) mode - "Save"/"Load" depending on what you want to do
+        2) DIR - Path to the corpus        
+        3) filename - 'abc.json', any name to pickle your corpus and save or load it.
+                       Must be present in the same location, else change the code to find the file
+        4) n - number of files to load in each subdirectory of the wiki corpus        
+    """
     
+    
+    # Global list variable for corpus sentences after processing
+    LAN_sentences = []
     if mode=="Load":
         # Reading data back
-	print "Loading English"
-        with open('engdata.json', 'r') as f:
-             ENG_sentences = ujson.load(f)
-        f.close()
-        print "Done with English"
-               
+       print "Loading Languge"
+       with open(filename, 'r') as f:
+             LAN_sentences = ujson.load(f)
+       f.close()
+       print "Language Loaded" 	
+  
+    print "Done with Language"
     if mode=="Save":
-        print "Going through English Wiki"            
-            
-        for subdir, dirs, files in os.walk(ENG_DIR):
-	   
-           for file in files[::5]:
-		
-                with codecs.open(os.path.join(subdir,file),'rb',encoding='utf-8') as engdoc:
-                    engcontent =engdoc.read()
-                    ENG_sentences += preprocess(engcontent)
-                
+        print "Going through the Wiki"
+        for subdir, dirs, files in os.walk(DIR):
+            for file in files[::n]:
+                with codecs.open(os.path.join(subdir,file),'rb',encoding='utf-8') as doc:
+                    content =doc.read()
+                    LAN_sentences += preprocess(content)
+        # Random shuffle of the sentences
+        shuffle(LAN_sentences)
         # Saving to file
-        print "Pickling English"
+        print "Pickling the corpus"
         
         # Writing JSON data
-        with open('engdata.json', 'w') as f:
-             ujson.dump(ENG_sentences, f)
+        with open(filename, 'w') as f:
+             ujson.dump(LAN_sentences, f)
         f.close()
-	print "Done saving"
-    return ENG_sentences    
-        
+    return LAN_sentences    
 
 # In[141]:
 
@@ -180,34 +173,34 @@ def ACS_sentences(ENG_sentences):
 #ENG_sentences = ACS_sentences(ENG_sentences)
 """
  
-def multilingualcorpus(mode,ENG_sentences,ESP_sentences):
+def multilingualcorpus(mode,LAN1_sentences,LAN2_sentences,filename):
     multilingual_data=[]
     if mode=="Save":
         
         # Assimilated Corpus of Multilingual texts        
-        multilingual_data= ENG_sentences + ESP_sentences
+        multilingual_data= LAN1_sentences + LAN2_sentences
         # Random shuffle of the sentences
         shuffle(multilingual_data)
         # Saving to file
         print "Pickling Multi-Corpus"
         
         # Writing JSON data
-        with open('multidata.json', 'w') as f:
+        with open(filename, 'w') as f:
              ujson.dump(multilingual_data, f)
         f.close()
 	print "Corpus saved"
         
     if mode=="Load":
-        # Reading data back
+    # Reading data back
 	print "Load Corpus"
-        with open('multidata.json', 'r') as f:
+        with open(filename, 'r') as f:
              multilingual_data = ujson.load(f)
         f.close()
         print "Done with Multi-Lingual Corpus"
     return multilingual_data
 
 
-def embeddings(ENG_sentences,ESP_sentences,multilingual_data):
+def embeddings(LAN_sentences,filename):
     """ 
     Using the word2vec implementation   
     - Initialize a model
@@ -217,90 +210,137 @@ def embeddings(ENG_sentences,ESP_sentences,multilingual_data):
     - window is the maximum distance between the current and predicted word within a sentence.
     - min_count => ignore all words with total frequency lower than this.
     
+    Taking the corpus in the required format or word2vec as LAN_sentences and saving it in a text file using filename
     """
-    # Persist a model to disk
-    
-    fname1=os.path.join(os.getcwd(),'word_vectors_eng.txt')
-    fname2=os.path.join(os.getcwd(),'word_vectors_esp.txt')
-    fname3=os.path.join(os.getcwd(),'word_vectors_mul.txt')
-    
     # Word2Vec
-    print "Saving English model"
-    model_eng = models.Word2Vec(ENG_sentences, size=300, window=5, min_count=5, workers=4)
-    model_eng.save(fname1)
-    print "Saving Spanish model"
-    model_esp = models.Word2Vec(ESP_sentences, size=300, window=5, min_count=5, workers=4)
-    model_esp.save(fname2)
-    print "Saving multi-lingual model"
-    model_mul = models.Word2Vec(multilingual_data, size=300, window=5, min_count=5, workers=4)
-    model_mul.save(fname3)
+    print "Saving Language model"
+    model_eng = models.Word2Vec(LAN_sentences, size=300, window=5, min_count=5, workers=4)
+    model_eng.save(filename)
+    print "Word2Vec embeddings complete"
+
+    
+def evaluate_embeddings(model,testfile):
+    """
+    Evaluating the Embeddings
+    
+    -Performing NLP word tasks with the model
+    -Probability of a text under the model
+    -Correlation with human opinion on word similarity and on analogies
+    -Question-words: Google have released their testing set of about 20,000 syntactic and semantic test examples, 
+    following the “A is to B as C is to D” task
+    - Doesn't match
+    - Most similar
+    
+    
+    """
+    print "Evaluating the Model"
+    accudict= model.accuracy(testfile)
+    
+    for i in range(len(accudict)):
+        print "For category ", accudict[i]['section'], "Accuracy is ", 100*float(len(accudict[i]['correct']))/(len(accudict[i]['incorrect'])+len(accudict[i]['correct']))
+    
+    
+    
+def corpus_stats(modelfile,testfile):
+    print "------Corpus Statistics------"
+    # Loading the models
+    model_lan = gensim.models.Word2Vec.load(modelfile)
+    vocab_lan = list(model_lan.wv.vocab.keys())
+    print "\n Vocabulary length of Corpus : ", len(vocab_lan)
+    print "Evaluating English embeddings "
+    evaluate_embeddings(model_lan,testfile)
 
 
+    
+# Persist a model to disk
+fname0 = os.path.join(os.getcwd(),'word_vectors_eng.txt')
+fname_comp = os.path.join(os.getcwd(),'word_vectors_eng_compress.txt')
 
-#ESP_sentences=spanishcorpus("Save")
-#ESP_sentences=spanishcorpus("Load")
+fname1 = os.path.join(os.getcwd(),'word_vectors_esp.txt')
+fname2 = os.path.join(os.getcwd(),'word_vectors_fre.txt')
+fname3 = os.path.join(os.getcwd(),'word_vectors_ita.txt')
 
-#ENG_sentences = englishcorpus("Save")
-#ENG_sentences = englishcorpus("Load")
+fnamex1 = os.path.join(os.getcwd(),'wv_enes_mul.txt')
+fnamex2 = os.path.join(os.getcwd(),'wv_enfr_mul.txt')
+fnamex3 = os.path.join(os.getcwd(),'wv_enit_mul.txt')
 
-#multilingual_data = multilingualcorpus("Save",ENG_sentences, ESP_sentences)
-#multilingual_data = multilingualcorpus("Load")
+fnameacs1 = os.path.join(os.getcwd(),'wv_enes_acs.txt')
+fnameacs2 = os.path.join(os.getcwd(),'wv_enfr_acs.txt')
+fnameacs3 = os.path.join(os.getcwd(),'wv_enit_acs.txt')
 
-#embeddings(ENG_sentences,ESP_sentences,multilingual_data)
-
-# In[36]:
+test_eng = os.path.join(os.getcwd(),'questions-words.txt')
+test_esp = os.path.join(os.getcwd(),'questions-words_esp.txt')
+test_ita = os.path.join(os.getcwd(),'questions-words_ita.txt')
+        
 """
-vocab = list(model.vocab.keys())
-vocab_len = len(vocab)
-vocab_len
+Uncomment the corresponding lines to process corpus or load data
 
+"""        
+#ESP_sentences = parsecorpus("Save",ESP_DIR,'espdata.json',5)
+#ESP_sentences = parsecorpus("Load",ESP_DIR,'espdata.json',5)
 
-# In[55]:
+#FRE_sentences = parsecorpus("Save",ESP_DIR,'fredata.json',5)
+#FRE_sentences = parsecorpus("Load",ESP_DIR,'fredata.json',5)
 
-st= vocab[448]
-st
+#ITA_sentences = parsecorpus("Save",ESP_DIR,'itadata.json',5)
+#ITA_sentences = parsecorpus("Load",ESP_DIR,'itadata.json',5)
 
+#ENG_sentences = parsecorpus("Save",ENG_DIR,'engdata.json',5)
+#ENG_sentences = parsecorpus("Load",ENG_DIR,'engdata.json',5)
 
-# In[56]:
-
-model[st]
-
-"""
-# In[ ]:
-
-"""
-Evaluating the Embeddings
-
--Performing NLP word tasks with the model
--Probability of a text under the model
--Correlation with human opinion on word similarity and on analogies
--Question-words: Google have released their testing set of about 20,000 syntactic and semantic test examples, 
-following the “A is to B as C is to D” task
-- Doesn't match
-- Most similar
-
+#ENG_sentences = parsecorpus("Save",ENG_DIR,'engdata_compress.json',8)
+#ENG_sentences = parsecorpus("Load",ENG_DIR,'engdata_compress.json',8)
 
 """
+Uncomment the corresponding lines to merge corpus
+
+""" 
+#multilingual_data = multilingualcorpus("Save",ENG_sentences, ESP_sentences,'en_es.json')
+#multilingual_data = multilingualcorpus("Load",[],[],'en_es.json')
+
+#multilingual_data = multilingualcorpus("Save",ENG_sentences, FRE_sentences,'en_fr.json')
+#multilingual_data = multilingualcorpus("Load",[],[],'en_fr.json')
+
+#multilingual_data = multilingualcorpus("Save",ENG_sentences, ITA_sentences,'en_it.json')
+#multilingual_data = multilingualcorpus("Load",[],[],'en_it.json')
 
 """
-# In[60]:
+Uncomment the corresponding lines to generate embeddings
 
-accudict= model.accuracy(os.path.join(os.getcwd(),'questions-words.txt'))
+""" 
 
-
-# In[81]:
-
-for i in range(len(accudict)):
-    print "For category ", accudict[i]['section'], "Accuracy is ", 100*float(len(accudict[i]['correct']))/(len(accudict[i]['incorrect'])+len(accudict[i]['correct']))
-
-
-
-# In[83]:
-
-model.most_similar(positive=['woman', 'king'], negative=['man'])
+#embeddings(ENG_sentences,fname0)
+#embeddings(ENG_sentences,fname_comp)
+#embeddings(ESP_sentences,fname1)
+#embeddings(FRE_sentences,fname2)
+#embeddings(ITA_sentences,fname3)
 
 
-# In[85]:
+#embeddings(multilingual_data,fnamex1)
+#embeddings(multilingual_data,fnamex2)
+#embeddings(multilingual_data,fnamex3)
 
-model.similarity('woman', 'man')
+#embeddings(multilingual_data,fnameacs1)
+#embeddings(multilingual_data,fnameacs2)
+#embeddings(multilingual_data,fnameacs3)
+
+
 """
+Uncomment the corresponding lines to print corpus statistics
+
+""" 
+
+#corpus_stats(fname0,test_eng)
+#corpus_stats(fname_comp,test_eng)
+
+#corpus_stats(fname1,test_esp)
+#corpus_stats(fnamex1,test_esp)
+#corpus_stats(fnameacs1,test_esp)
+
+#corpus_stats(fname3,test_ita)
+#corpus_stats(fnamex3,test_ita)
+#corpus_stats(fnameacs3,test_ita)
+
+
+
+
